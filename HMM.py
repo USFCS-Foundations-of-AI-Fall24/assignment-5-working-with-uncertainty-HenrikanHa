5da71b7718aaef13b1e8490bd7939288520850c2
Thanh Cong Nguyen <tnguyen100@dons.usfca.edu>
2024-11-14 15:27:06 -0800

    Implementing HMM

--- HMM.py
+++ HMM.py
@@ -93,27 +93,26 @@ class HMM:
         M[state_index['#'], 0] = 1.0
         for state in state_values:
             if state != '#':
-                M[state_index[state], 0] = 0
-                T_start = self.transitions['#'].get(state, 0)
-                E_emit = self.emissions[state].get(sequence[0], 0)
+                M[state_index[state], 0] = 0.0
+                T_start = self.transitions['#'].get(state, 0.0)
+                E_emit = self.emissions[state].get(sequence[0], 0.0)
                 M[state_index[state], 1] = T_start * E_emit
 
         for t in range(2, num_observations + 1):
             for state in state_values:
                 if state != '#':
-                    sum_prob = 0
-                    E_emit_prev = self.emissions[state].get(sequence[t - 1], 0)
+                    sum_prob = 0.0
+                    E_emit_prev = self.emissions[state].get(sequence[t - 1], 0.0)
                     for state2 in state_values:
                         if state2 != '#':
                             prev_prob = M[state_index[state2], t - 1]
-                            T_trans = self.transitions[state2].get(state, 0)
+                            T_trans = self.transitions[state2].get(state, 0.0)
                             sum_prob += prev_prob * T_trans * E_emit_prev
                     M[state_index[state], t] = sum_prob
 
         final_probs = M[:, -1]
         most_likely_state_index = numpy.argmax(final_probs)
         most_likely_state = state_values[most_likely_state_index]
-
         if domain == "lander":
             safe_landings = {"2,5", "3,4", "4,3", "4,4", "5,5"}
             is_safe = "Yes" if most_likely_state in safe_landings else "No"
@@ -122,7 +121,7 @@ class HMM:
         ## you do this: Implement the Viterbi algorithm. Given a Sequence with a list of emissions,
         ## determine the most likely sequence of states.
 
-    def viterbi(self, sequence):
+    def viterbi(self, sequence, domain=""):
         state_values = list(self.transitions.keys())
         num_states = len(state_values)
         num_observations = len(sequence)
@@ -135,22 +134,22 @@ class HMM:
         M[state_index['#'], 0] = 1.0
         for state in state_values:
             if state != '#':
-                M[state_index[state], 0] = 0
-                T_start = self.transitions['#'].get(state, 0)
-                E_emit = self.emissions[state].get(sequence[0], 0)
+                M[state_index[state], 0] = 0.0
+                T_start = self.transitions['#'].get(state, 0.0)
+                E_emit = self.emissions[state].get(sequence[0], 0.0)
                 M[state_index[state], 1] = T_start * E_emit
 
         for t in range(2, num_observations + 1):
             for state in state_values:
                 if state != '#':
-                    E_emit = self.emissions[state].get(sequence[t - 1], 0)
-                    max_val = 0
+                    E_emit = self.emissions[state].get(sequence[t - 1], 0.0)
+                    max_val = 0.0
                     best_prev_state = 0
 
                     for state2 in state_values:
                         if state2 != '#':
                             M_prev = M[state_index[state2], t - 1]
-                            T_trans = self.transitions[state2].get(state, 0)
+                            T_trans = self.transitions[state2].get(state, 0.0)
                             val = M_prev * T_trans * E_emit
                             if val > max_val:
                                 max_val = val
@@ -164,6 +163,12 @@ class HMM:
         for t in range(num_observations, 1, -1):
             best_final_state_index = Backpointers[best_final_state_index, t]
             most_likely_sequence.insert(0, state_values[best_final_state_index])
+
+        if domain == "lander":
+            safe_landings = {"2,5", "3,4", "4,3", "4,4", "5,5"}
+            is_safe = "Yes" if most_likely_sequence[-1] in safe_landings else "No"
+            return most_likely_sequence, is_safe
+
         return most_likely_sequence
     ## You do this. Given a sequence with a list of emissions, fill in the most likely
     ## hidden states using the Viterbi algorithm.
@@ -190,9 +195,8 @@ def main():
     if args.forward:
         with open(args.forward, 'r') as f:
             sequence = f.read().strip().split()
-
+        print("Observation sequence:", ' '.join(sequence) + "\n")
         result = h.forward(sequence, domain=args.basename)
-
         if args.basename == "lander":
             most_likely_state, is_safe = result
             print(f"Most likely final state: {most_likely_state}")
@@ -203,9 +207,14 @@ def main():
     if args.viterbi:
         with open(args.viterbi, 'r') as f:
             sequence = f.read().strip().split()
-
-        most_likely_sequence = h.viterbi(sequence)
-        print("Most likely sequence of states:", ' '.join(most_likely_sequence))
+        print("Observation sequence:", ' '.join(sequence) + "\n")
+        result = h.viterbi(sequence, domain=args.basename)
+        if args.basename == "lander":
+            most_likely_sequence, is_safe = result
+            print("Most likely sequence of states:", ' '.join(most_likely_sequence))
+            print(f"Safe to land: {is_safe}")
+        else:
+            print("Most likely sequence of states:", ' '.join(result))
 
 if __name__ == "__main__":
     main()
